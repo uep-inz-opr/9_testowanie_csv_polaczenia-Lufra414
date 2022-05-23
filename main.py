716dd5c18878555e19ca14eecbe90dfc642a78c7
unknown <lfrackowiak4@gmail.com>
2022-05-23 19:25:02 +0200

    kolejna

--- main.py
+++ main.py
@@ -19,4 +19,4 @@ class MenadzerPolaczen:
   def pobierz_najczesciej_dzwoniacego(self):
     return max(self.data_dict.items(), key= lambda x: x[1])
     if __name__ == '__main__':
-print(Polaczenia(input()).pobierz_najczesciej_dzwoniacego())
+    print(Polaczenia(input()).pobierz_najczesciej_dzwoniacego())
