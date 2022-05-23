6baff4dcb53ebe0f75fe149deb30bf82e629c59b
unknown <lfrackowiak4@gmail.com>
2022-05-23 19:34:33 +0200

    kolejna

--- main.py
+++ main.py
@@ -24,5 +24,9 @@ class MenadzerPolaczen:
 
   def pobierz_najczesciej_dzwoniacego(self):
     return max(self.data_dict.items(), key= lambda x: x[1])
-    if __name__ == '__main__':
-    print(Polaczenia(input()).pobierz_najczesciej_dzwoniacego())
+
+if __name__ == "__main__":
+  nazwa_pliku = input()
+  mp = MenadzerPolaczen(input)
+  wynik = mp.pobierz_najczesciej_dzwoniacego()
+  print (wynik)
